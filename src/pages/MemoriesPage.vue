1af75051a4976528f504a9fc347bb0a6cbf9c309
Logan Edmonds <logantedmonds@gmail.com>
2022-12-02 17:27:31 -0600

    simple list

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <base-layout page-title="All Memories">
+  
+  </base-layout>
+    <ion-list>
+      <ion-item>A trip into the mountains</ion-item>
+      <ion-item>Surfing the sea side</ion-item>
+      <ion-item>Good eating</ion-item>
+    </ion-list>
+</template>
+
+<script>
+import { IonList, IonItem } from '@ionic/vue'
+
+export default {
+  components: {
+    IonList,
+    IonItem
+  }
+}
+</script>
